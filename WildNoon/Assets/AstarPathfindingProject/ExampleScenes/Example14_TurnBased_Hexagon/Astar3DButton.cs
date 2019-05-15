23862c2ba0e7795f515fe511a64a1be792f807b6
Zoulouti <Pauldelaubadere@gmail.com>
2019-05-15 11:36:13 +0200

    paul check

--- WildNoon/Assets/AstarPathfindingProject/ExampleScenes/Example14_TurnBased_Hexagon/Astar3DButton.cs
+++ WildNoon/Assets/AstarPathfindingProject/ExampleScenes/Example14_TurnBased_Hexagon/Astar3DButton.cs
@@ -6,13 +6,24 @@ namespace Pathfinding.Examples {
 	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_examples_1_1_astar3_d_button.php")]
 	public class Astar3DButton : MonoBehaviour {
 		public GraphNode node;
+        public int cost;
 
-		public void OnHover (bool hover) {
+
+		public void OnHover (TurnBasedAI unit)
+        {
+            // TODO: Play animation
+        }
+
+		public int OnClick () {
+
+            return cost;
 			// TODO: Play animation
 		}
 
-		public void OnClick () {
-			// TODO: Play animation
-		}
-	}
+        private void Update()
+        {
+            
+        }
+
+    }
 }
